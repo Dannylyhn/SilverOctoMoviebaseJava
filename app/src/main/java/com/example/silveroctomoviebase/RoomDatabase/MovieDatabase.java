034cec1aee7f2ca3d4d4af7f6c070c86e56215de
Danny Ly <dannylyhn@gmail.com>
2022-12-18 18:59:18 +0100

    Set version back to 1

--- app/src/main/java/com/example/silveroctomoviebase/RoomDatabase/MovieDatabase.java
+++ app/src/main/java/com/example/silveroctomoviebase/RoomDatabase/MovieDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 
-@Database(entities = {Movie.class}, version = 2)
+@Database(entities = {Movie.class}, version = 1)
 public abstract class MovieDatabase extends RoomDatabase {
 
     public abstract MovieDao movieDao();
